2f81cab30dd9281a17c26f9d62d1b71233520137
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-02-05 20:54:50 -0500

    Add files via upload

---
+++ pages/client.js
@@ -0,0 +1,49 @@
+import Layout from '../components/layout'
+var fetch = require('node-fetch')
+
+// async function retrieveToken(req, res) {
+//   const token = await jwt.getToken({ req, secret })
+//   return JSON.stringify(token, null, 2)
+// }
+
+export default function Form() {
+  const registerUser = async event => {
+    event.preventDefault()
+
+    const get = await fetch('/api/examples/jwt', {
+      method: 'GET',
+    });
+    
+    const token = await get.json()
+    console.log(token.accessToken)
+
+    const res = await fetch(
+      'https://bitcampdev.herokuapp.com/api/post',
+      {
+        body: JSON.stringify({
+          accesstoken: token.accessToken,
+          lab: event.target.name.value
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        method: 'POST'
+      }
+    )
+
+    const result = await res.json()
+    console.log(result)
+    // result.user => 'Ada Lovelace'
+  }
+
+  return (
+    <Layout>
+    <form onSubmit={registerUser}>
+      <label htmlFor="name">Bitlab   </label>
+      <input id="name" name="name" type="text" autoComplete="name" required />
+      <button type="submit">Register</button>
+    </form>
+    </Layout>
+  )
+}
